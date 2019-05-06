f252baefda40606d3a45117bafaccd9eee660344
Nicholas Comeau <nick_comeau3@hotmail.com>
2019-05-06 17:54:11 -0300

    Some Stuff happened

---
+++ ProjectDemonInvasion/Assets/Scripts/General/PlayGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour {
+
+    public void PlayTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
